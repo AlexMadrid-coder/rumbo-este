6d520e7c97c30cbd65baf234c1d4a96dea255c5f
Alejandro Madrid <91757705+AlexMadrid-coder@users.noreply.github.com>
2025-08-15 04:03:42 +0200

    commit 4am

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app"> </div>
+    <!-- Aquí meto los componentes de la web-->
+    <Navigation />
+    
+</template>
+
+<script>
+// aquí importo toods los componentes que se van a utilizar
+
+export default {
+  name: 'App',
+  // aquí añadimos toodos los compoentntes importados
+  components: {
+    Navigation
+  }
+}
+
+</script>
+
+<style>
+@import './assets/styles/styles.css';
+@import './assets/styles/components.css';
+</style>
---
+++ src/components/layout/navigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar" :class="{ scrolled: isScrolled }">
+        <div class="container">
+            <!-- Aquí ponemos el nombre de la banda-->
+            <div class="nav-container">
+                <h3>RUMBO ESTE</h3>
+            </div>
+            <!-- Aquí ponemos toda la barra de navegaci´pn-->
+             <ul class="nav-menu" :class="{ activate: isMenuOpen }">
+                <li class="nav-item"> 
+                    <a href="#inicio" class="nav-link" @click="closeMenu">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#musica" class="nav-link" @click="closeMenu">Música</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#banda" class="nav-link" @click="closeMenu">Banda</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#conciertos" class="nav-link" @click="closeMenu">Conciertos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#contacto" class="nav-link" @click="closeMenu">Contacto</a>
+                </li>
+             </ul>
+             <!-- Botón de hamburguesa para el diseño móvil-->
+              <div class="hamburguer" :class="{ active: isMenuOpen }" @click="toggleMenu"> 
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+              </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+
+</script>
+
+<style scoped>
+/* Estilos específicos para el componente de navegación */
+</style>
